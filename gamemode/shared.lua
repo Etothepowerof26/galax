24271f626a0f536c0b5887917ef41452ea73e730
TwentySix <refertrionjared1@gmail.com>
2018-11-04 20:42:18 -0500

    [6] Changes to file structure, and basically completed the 'Init' function.
    Oh, so thats why I added "shared.lua"
    
    Also added some ASCII art.

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,4 +1,4 @@
-GM.Name = "galax"
+GM.Name = "Galax"
 GM.Author = ""
 DeriveGamemode("sandbox")
 function GM:Initialize()
@@ -7,17 +7,92 @@ end
 
 module("galax", package.seeall)
 
+--[[
+
+   ▄██████▄     ▄████████  ▄█          ▄████████ ▀████    ▐████▀ 
+  ███    ███   ███    ███ ███         ███    ███   ███▌   ████▀  
+  ███    █▀    ███    ███ ███         ███    ███    ███  ▐███    
+ ▄███          ███    ███ ███         ███    ███    ▀███▄███▀    
+▀▀███ ████▄  ▀███████████ ███       ▀███████████    ████▀██▄     
+  ███    ███   ███    ███ ███         ███    ███   ▐███  ▀███    
+  ███    ███   ███    ███ ███▌    ▄   ███    ███  ▄███     ███▄  
+  ████████▀    ███    █▀  █████▄▄██   ███    █▀  ████       ███▄ 
+                          ▀                                      
+]]
+
+-- Logging system for Galax.
+-- Basically a fancy way of printing.
+Log = function(Head, Message)
+	MsgC(Color(0, 127, 255), "[GALAX] ", string.upper(Head), " ", Color(255, 255, 255))
+	print(Message)
+end
+
+-- ultimate hakr debug mode yes
+DebugMode = false
+
+-- File location variables.
+AddonsFileLoc = "galax/gamemode/galax_addons"
 FileLoc = "LUA"
+
+-- File functions (basically bad code)
+FileFuncs = {
+	["sh_"] = function(FilePath, File)
+		if (SERVER) then
+			AddCSLuaFile(FilePath)
+		else
+			include(File)
+		end
+	end,
+	["sv_"] = function(FilePath, File)
+		if (SERVER) then
+			include(File)
+		end
+	end,
+	["cl_"] = function(FilePath, File)
+		if (SERVER) then
+			AddCSLuaFile(FilePath)
+			include(File)
+		else
+			include(File)
+		end
+	end
+}
+
+-- Initializes Galax.
 Init = function(NewFilePath)
 	-- Loads the files based on name.
 	-- If it cannot find any identifier, it runs it shared.
 	-- Will also be recursive.
+	local FPath
+	local Files, Directories
 	
-	if not NewFilePath then
-		
+	if (not NewFilePath) then
+		FPath = AddonsFileLoc .. "/*"
 	else
+		FPath = NewFilePath .. "/*"
+	end
 	
+	Files, Directories = file.Find(FPath .. "/*", FileLoc)
+	
+	table.foreach(Files, function(k, v)
+		local Prefix = v:sub(1, 3)
+		if (FileFuncs[Prefix]) then
+			FileFuncs[Prefix](AddonsFileLoc .. "/" .. v, v)
+		else
+			FileFuncs["sh_"](AddonsFileLoc .. "/" .. v, v)
+		end
+	end)
+	
+	if (not next(Directories) == nil) then
+		table.foreach(Directories, function(k, v)
+			Init(AddonsFileLoc .. "/" .. v)
+		end)
 	end
 end
 
-hook.Add("InitPostEntity", "galax", Init)
+-- Hooks it to InitPostEntity.
+hook.Add("InitPostEntity", "galax", function()
+	Init()
+	
+	Log("init", SERVER and "Loaded on the server!" or "Loaded on the client!")
+end)
