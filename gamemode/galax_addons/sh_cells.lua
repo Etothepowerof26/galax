c3a84b0de1cb46caee4c36e0fd56d84b899c62d8
Etothepowerof26 <refertrionjared1@gmail.com>
2018-11-06 14:42:25 -0500

    [15] Create base for sh_cells

--- gamemode/galax_addons/sh_cells.lua
+++ gamemode/galax_addons/sh_cells.lua
@@ -1,1 +1,14 @@
 module("galax", package.seeall)
+
+function Meta.Player:IsInCell()
+        -- Since theres nothing for right now, return false.
+        return false
+end
+
+function Meta.Player:CanBuildInCell()
+        if (self:IsInCell()) then
+                -- Since theres nothing for right now, return false.
+                return false
+        end
+        return false
+end
